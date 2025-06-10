883a3e7f35154f08947e8c428fc965c9e9322724
k1fir4ig <maxim140906@gmail.com>
2025-06-10 23:30:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int main() {
+    int arr[] = {10, 20, 30, 30, 40, 40, 50};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int value = 70;
+
+    std::cout << "Method 1: " << countPairs1(arr, len, value) << std::endl;
+    std::cout << "Method 2: " << countPairs2(arr, len, value) << std::endl;
+    std::cout << "Method 3: " << countPairs3(arr, len, value) << std::endl;
+
     return 0;
 }
